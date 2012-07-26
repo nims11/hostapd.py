cc599b5c1d25196eb23cdde5ebd44ea4ec483798
Nimesh Ghelani <nimeshghelani@gmail.com>
2012-07-26 23:57:08 +0530

    interactive mode, exit

--- hostapd.py
+++ hostapd.py
@@ -1,2 +1,51 @@
 #!/usr/bin/env python2.7
+import sys
+from HPS import start_hostapd
 
+def exit_script():
+	print '\nGoodbye!'
+	sys.exit()
+def interactive():
+	"""
+	Starts Interactive Session
+	"""
+	print 'Interactive Session'
+	print
+	print '1. Start Hostapd'
+	print '2. Stop Hostapd'
+	print '3. Configure hostapd.conf'
+	print '0. exit'
+	while True:
+		try:
+			print 'Enter Choice : ',
+			ch = int(raw_input())
+			break;
+		except KeyboardInterrupt:
+			exit_script()
+		except:
+			continue;
+
+	options = { 0 : exit_script,
+			1 : start_hostapd,
+			#2 : stop_hostapd,
+			#3 : config_hostapd,
+			}
+	while ch not in options:
+		print 'Invalid Option'
+		try:
+			print 'Enter Choice : ',
+			ch = int(raw_input())
+		except KeyboardInterrupt:
+			exit_script()
+		except:
+			continue;
+	
+	options[ch]();
+
+
+def main():
+	if len(sys.argv) == 1:
+		interactive()
+
+if __name__ == '__main__':
+	main()
