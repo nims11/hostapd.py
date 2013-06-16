5858d26c73e760a910bd81b15a1ffb967834ade8
Nimesh Ghelani <nimeshghelani@gmail.com>
2013-06-16 22:39:01 +0530

    Hostapd stop working

--- HPS.py
+++ HPS.py
@@ -26,19 +26,34 @@ def start_hostapd():
 			print 'Done!'
 		else:
 			print 'Failed!'
-			exit_error('[ERROR] Failed to initiate %s, check log file %s' % (section, conf[section]['LOGFILE']))
+			exit_error('[ERROR] Failed to initiate [%s], check log file %s' % (section, conf[section]['LOGFILE']))
+		
 
 
 def stop_hostapd():
 	"""
 	Stops Hostapd and dhcpd
 	"""
-	print
-	print 'Killing Hostapd...'
-	subprocess.call(['killall','hostapd'])
-	print 'Killing dhcpd...'
-	subprocess.call(['killall','dhcpd'])
-	print
+	conf = config_gen.get_config()
+	env_tups = [(section+'_'+key, val) for section in conf.keys() for key, val in conf[section].items()]
+	env_dict = dict(os.environ.items() + env_tups)
+
+	print 'Stopping...'
+	for section in config.script_order[::-1]:
+		if conf[section].has_key('EXIT_SCRIPT'):
+			print 'Executing %s for [%s]...' % (conf[section]['EXIT_SCRIPT'], section)
+			ret = subprocess.call(conf[section]['EXIT_SCRIPT'], env=env_dict)
+			if ret == 0:
+				print 'Done!'
+			else:
+				print 'Failed!'
+				exit_error('[ERROR] Failed to exit [%s], check log file %s' % (section, conf[section]['LOGFILE']))
+#	print
+#	print 'Killing Hostapd...'
+#	subprocess.call(['killall','hostapd'])
+#	print 'Killing dhcpd...'
+#	subprocess.call(['killall','dhcpd'])
+#	print
 
 def restart_hostapd():
 	stop_hostapd()
